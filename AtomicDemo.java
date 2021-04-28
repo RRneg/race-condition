9626f735173b5a7a4185a4d0bd12171cf78f8546
Oleksandr Minaiev <71498446+RRneg@users.noreply.github.com>
2021-04-28 14:27:33 +0300

    Add files via upload

---
+++ AtomicDemo.java
@@ -0,0 +1,30 @@
+package AtomicDemo;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicDemo {
+    public static void main(String[] args) {
+        new AtomThread("A");
+        new AtomThread("B");
+        new AtomThread("C");
+    }
+}
+
+class Shared{
+    static AtomicInteger ai = new AtomicInteger(0);
+}
+
+class AtomThread implements Runnable{
+    String name;
+    AtomThread(String n){
+        name = n;
+        new Thread(this).start();
+    }
+
+    public void run(){
+        System.out.println("Запуск потока " + name);
+        for (int i = 1; i <= 3; i++)
+            System.out.println("Поток " + name + " получено: " + Shared.ai.getAndSet(i));
+
+    }
+}
